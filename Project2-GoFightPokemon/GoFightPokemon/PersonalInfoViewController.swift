f6bd6db4ecb10787d0bb1e3e457bc7c9ebb41d95
WeiTsungCheng <sanctuary85c@gmail.com>
2017-08-01 14:06:53 +0800

    collection3

--- Project2-GoFightPokemon/GoFightPokemon/PersonalInfoViewController.swift
+++ Project2-GoFightPokemon/GoFightPokemon/PersonalInfoViewController.swift
@@ -90,7 +90,6 @@ class PersonalInfoViewController: UIViewController {
 
         //儲存相簿選擇的照片到fireBase
         // 當selectedPhoto有東西時，將照片上傳
-
         //判定如果image 為預設原圖不要上傳
         if headPhoto.image != #imageLiteral(resourceName: "icons8-Lion Head Filled-50") {
 
@@ -407,6 +406,11 @@ extension PersonalInfoViewController : UITextFieldDelegate {
 }
 
 
+
+
+
+
+
 //處理UIImagePickerControll
 extension PersonalInfoViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -422,12 +426,8 @@ extension PersonalInfoViewController: UIImagePickerControllerDelegate, UINavigat
 
         headPhoto.contentMode = UIViewContentMode.scaleAspectFit
 
-
-
-
-
-
     }
+
 
 
 
