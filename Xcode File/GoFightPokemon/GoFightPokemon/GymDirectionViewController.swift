ebd67020890f14f46f1b0797015bea126bf8dd4d
WeiTsungCheng <sanctuary85c@gmail.com>
2017-08-14 14:13:41 +0800

    direction2

--- Xcode File/GoFightPokemon/GoFightPokemon/GymDirectionViewController.swift
+++ Xcode File/GoFightPokemon/GoFightPokemon/GymDirectionViewController.swift
@@ -13,6 +13,7 @@ import SwiftyJSON
 import Alamofire
 
 class GymDirectionViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
+
     @IBOutlet weak var googleMaps: UIImageView!
 
     override func viewDidLoad() {
@@ -26,15 +27,5 @@ class GymDirectionViewController: UIViewController, GMSMapViewDelegate, CLLocati
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
